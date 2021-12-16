5d8558cd2419747791d79e423ea0f89cbaef78ca
unknown <tuonghai.contact@gmail.com>
2021-12-16 19:57:28 +0700

    API post anh san pham

---
+++ Laptop-store-e-commerce (Server)/Laptop-store-e-comerce/Controllers/PictureController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Laptop_store_e_comerce.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Laptop_store_e_comerce.Controllers
+{
+    [Route("data/[controller]")]
+    [ApiController]
+    public class PictureController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+        private readonly StoreContext database;
+        public PictureController(IWebHostEnvironment hostEnvironment, StoreContext _context)
+        {
+            this.hostEnvironment = hostEnvironment;
+            this.database = _context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> postImage([FromForm] Image image)
+        {
+            var idPro = image.idProduct;
+            if (!database.Products.Any(pro => pro.Id == image.idProduct)) return NotFound();
+            try
+            {
+                var pro = await database.Products.FirstOrDefaultAsync(pro => pro.Id == image.idProduct);
+                pro.Nameimage = new String(Path.GetFileName(image.file.FileName));
+                database.Entry(pro).State = EntityState.Modified;
+                database.SaveChangesAsync();
+                return await saveImage(image.file);
+            }
+            catch (Exception) { return BadRequest(); }
+        }
+        public async Task<ActionResult> saveImage(IFormFile imageFile)
+        {
+            //string imageName = new String(Path.GetFileNameWithoutExtension(image.file.FileName).Take(10).ToArray()).Replace(' ', '-');
+            string imageName = new String(Path.GetFileName(imageFile.FileName));
+            //imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(image.file.FileName);
+            var imagePath = Path.Combine(hostEnvironment.ContentRootPath, "Images/Products", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            { await imageFile.CopyToAsync(fileStream); }
+            return NoContent();
+        }
+        public class Image
+        {
+            public string idProduct { get; set; }
+            public IFormFile file { get; set; }
+        }
+    }
+}
---
+++ Laptop-store-e-commerce (Server)/Laptop-store-e-comerce/Controllers/PostAnhController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laptop_store_e_comerce.Controllers
+{
+    [Route("data/[controller]")]
+    [ApiController]
+    public class PostAnhController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+        public PostAnhController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+        [HttpPost]
+        public async Task<ActionResult> postImage([FromForm]Image image)
+        {
+            var a = image.idProduct;
+            //string imageName = new String(Path.GetFileNameWithoutExtension(image.file.FileName).Take(10).ToArray()).Replace(' ', '-');
+            string imageName = new String(Path.GetFileName(image.file.FileName));
+            //imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(image.file.FileName);
+            var imagePath = Path.Combine(hostEnvironment.ContentRootPath, "Images/Panels", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            { await image.file.CopyToAsync(fileStream); }
+            return NoContent();
+        }
+        public class Image
+        {
+            public string idProduct { get; set; }
+            public IFormFile file { get; set; }
+    }
+    }
+}
